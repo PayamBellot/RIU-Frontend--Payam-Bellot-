e90f3008f1549b9756dd4c49affb6dd0e2707d45
pbellot <payam.bellot@external.atlascopco.com>
2025-03-26 20:27:53 +0100

    fixed an error where when adding heroes or modyfing, the last letter was lower case

--- src/app/directives/uppercase.directive.ts
+++ src/app/directives/uppercase.directive.ts
@@ -6,10 +6,13 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class UppercaseDirective {
   constructor(private el: ElementRef) {}
 
-  @HostListener('input', ['$event'])
-  onInput(event: Event): void {
-    const input = this.el.nativeElement as HTMLInputElement;
-    const value = input.value;
-    input.value = value.toUpperCase();
+  @HostListener('input', ['$event']) onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const start = input.selectionStart;
+    const end = input.selectionEnd;
+    input.value = input.value.toUpperCase();
+    input.dispatchEvent(new Event('input', { bubbles: true }));
+
+    input.setSelectionRange(start, end);
   }
 }

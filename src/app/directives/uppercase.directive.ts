3987827c197f0457b0ad625248052a7ed422b2b3
pbellot <payam.bellot@external.atlascopco.com>
2025-03-27 14:51:21 +0100

    fixed a recursive error in uppercase directive

--- src/app/directives/uppercase.directive.ts
+++ src/app/directives/uppercase.directive.ts
@@ -8,11 +8,13 @@ export class UppercaseDirective {
 
   @HostListener('input', ['$event']) onInput(event: Event) {
     const input = event.target as HTMLInputElement;
-    const start = input.selectionStart;
-    const end = input.selectionEnd;
-    input.value = input.value.toUpperCase();
-    input.dispatchEvent(new Event('input', { bubbles: true }));
-
-    input.setSelectionRange(start, end);
+    const newInput = input.value.toUpperCase();
+    if (input.value !== newInput) {
+      const start = input.selectionStart;
+      const end = input.selectionEnd;
+      input.value = newInput;
+      input.dispatchEvent(new Event('input', { bubbles: true }));
+      input.setSelectionRange(start, end);
+    }
   }
 }
